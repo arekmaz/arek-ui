8b7c035294d7d9383afc7e0c5f5abc99e56eb3dd
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2023-12-29 18:41:24 +0100

    --wip-- [skip ci]

--- app/components/utils/create-style-context.tsx
+++ app/components/utils/create-style-context.tsx
@@ -10,6 +10,7 @@ import {
   type JSX,
   useMemo,
 } from "react";
+import { cn } from "./cn";
 
 type GenericProps = Record<string, unknown>;
 type StyleRecipe = {
@@ -18,17 +19,19 @@ type StyleRecipe = {
   variantKeys: (string | number)[];
 };
 type StyleSlot<R extends StyleRecipe> = keyof ReturnType<R>;
+type Classes<R extends StyleRecipe> = { [key in StyleSlot<R>]?: string };
 type StyleSlotRecipe<R extends StyleRecipe> = Record<StyleSlot<R>, string>;
 type StyleVariantProps<R extends StyleRecipe> = Parameters<R>[0];
 type CombineProps<T, U> = Omit<T, keyof U> & U;
 
-const cx = (...args: (string | undefined)[]) => args.filter(Boolean).join(" ");
-
 export interface ComponentVariants<
   T extends ElementType,
-  R extends StyleRecipe
+  R extends StyleRecipe,
+  OtherProps
 > {
-  (props: CombineProps<ComponentProps<T>, StyleVariantProps<R>>): JSX.Element;
+  (
+    props: CombineProps<ComponentProps<T>, StyleVariantProps<R>> & OtherProps
+  ): JSX.Element;
 }
 
 export const splitVariantProps = (
@@ -50,36 +53,49 @@ export const splitVariantProps = (
 };
 
 export const createStyleContext = <R extends StyleRecipe>(recipe: R) => {
-  const StyleContext = createContext<StyleSlotRecipe<R> | null>(null);
+  const StyleContext = createContext<{
+    slotStyles: StyleSlotRecipe<R> | null;
+    classes: Classes<R>;
+  }>({ slotStyles: null, classes: {} });
 
   const withProvider = <T extends ElementType>(
     Component: T,
     slot?: StyleSlot<R>
-  ): ComponentVariants<T, R> => {
-    const StyledComponent = forwardRef((props: ComponentProps<T>, ref) => {
-      const [variantProps, otherProps] = splitVariantProps(
-        recipe.variantKeys,
-        props
-      );
+  ): ComponentVariants<T, R, { classes?: Classes<R> }> => {
+    const StyledComponent = forwardRef(
+      (
+        {
+          classes = {},
+          ...props
+        }: ComponentProps<T> & {
+          classes?: Classes<R>;
+        },
+        ref
+      ) => {
+        const [variantProps, otherProps] = splitVariantProps(
+          recipe.variantKeys,
+          props
+        );
 
-      const slotStyles = useMemo(
-        () => recipe(variantProps) as any,
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        Object.values(variantProps)
-      );
-      return (
-        <StyleContext.Provider value={slotStyles}>
-          <Component
-            ref={ref}
-            {...otherProps}
-            className={slotStyles[slot]({
-              className: otherProps.className,
-            })}
-          />
-        </StyleContext.Provider>
-      );
-    });
-    return StyledComponent as unknown as ComponentVariants<T, R>;
+        const slotStyles = useMemo(
+          () => recipe(variantProps) as any,
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          Object.values(variantProps)
+        );
+        return (
+          <StyleContext.Provider value={{ slotStyles, classes }}>
+            <Component
+              ref={ref}
+              {...otherProps}
+              className={slotStyles[slot]({
+                className: cn(classes[slot], props.className),
+              })}
+            />
+          </StyleContext.Provider>
+        );
+      }
+    );
+    return StyledComponent as any;
   };
 
   const withContext = <T extends ElementType>(
@@ -87,11 +103,14 @@ export const createStyleContext = <R extends StyleRecipe>(recipe: R) => {
     slot?: StyleSlot<R>
   ): T => {
     if (!slot) return Component;
+
     const StyledComponent = forwardRef((props: ComponentProps<T>, ref) => {
-      const slotStyles = useContext(StyleContext) as any;
+      const { slotStyles, classes } = useContext(StyleContext) as any;
       return createElement(Component, {
         ...props,
-        className: slotStyles?.[slot]({ className: props.className }),
+        className: slotStyles?.[slot]({
+          className: cn(classes[slot], props.className),
+        }),
         ref,
       });
     });
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useState } from "react";
 import { Checkbox } from "~/components/ui/checkbox_1";
+import { cn } from "~/components/utils/cn";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,6 +11,7 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [checked, setChecked] = useState(false);
   return (
     <div>
       <h1>Welcome to Remix</h1>
@@ -17,8 +20,18 @@ export default function Index() {
       <Checkbox checked="indeterminate" />
       <Checkbox checked />
       <Checkbox checked={false} /> */}
-      <Checkbox.Root>
+      <Checkbox.Root
+        classes={{ root: cn({ "text-blue-500": checked }) }}
+        checked={checked}
+        onCheckedChange={(c) => setChecked(c.checked as boolean)}
+      >
         <Checkbox.Label className="text-red-500">label</Checkbox.Label>
+        <Checkbox.Control>
+          <Checkbox.CheckIcon />
+        </Checkbox.Control>
+        <Checkbox.Control>
+          <Checkbox.CheckIcon />
+        </Checkbox.Control>
         <Checkbox.Control>
           <Checkbox.CheckIcon />
         </Checkbox.Control>
