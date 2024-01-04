8ad59a3b0824e20b8047a5e86351d1e8d48409ab
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-01-04 22:47:10 +0100

    --wip-- [skip ci]

--- app/demo/tagsInput.stories.tsx
+++ app/demo/tagsInput.stories.tsx
@@ -1,8 +1,13 @@
 import { TagsInput } from "~/components/ui/tags-input";
+import { Combobox as C } from "~/components/ui/combobox";
 import { Story } from "./storyHelpers";
-import { ArrowRight, PiIcon, XIcon } from "lucide-react";
+import { ArrowRight, ChevronsUpDownIcon, PiIcon, XIcon } from "lucide-react";
 import { IconButton } from "~/components/ui/icon-button";
 import { InputGroup } from "~/components/ui/input-group";
+import { useState } from "react";
+import { matchSorter } from "match-sorter";
+import { data } from "~/components/ui/select";
+import { Portal } from "@ark-ui/react";
 
 const Normal = () => {
   return (
@@ -66,6 +71,76 @@ const WithInputGroup = () => {
   );
 };
 
+const WithCombobox = () => {
+  const [items, setItems] = useState(data);
+
+  const handleChange = (e: any) => {
+    const filtered = matchSorter(data, e.value, { keys: ["label"] });
+    setItems(filtered.length > 0 ? filtered : data);
+  };
+
+  return (
+    <TagsInput>
+      {(api) => (
+        <>
+          <TagsInput.Label>With combobox</TagsInput.Label>
+          <TagsInput.Control>
+            {api.value.map((value, index) => (
+              <TagsInput.Item key={index} index={index} value={value}>
+                <TagsInput.ItemInput />
+                <TagsInput.ItemText>{value}</TagsInput.ItemText>
+                <TagsInput.ItemDeleteTrigger asChild>
+                  <IconButton size="sm" variant="ghost">
+                    <XIcon />
+                  </IconButton>
+                </TagsInput.ItemDeleteTrigger>
+              </TagsInput.Item>
+            ))}
+
+            <C
+              items={items}
+              onInputValueChange={handleChange}
+              onValueChange={({ value: [firstValue] }) => {
+                console.log({ firstValue, v: Array.from(api.value) });
+                if (firstValue) {
+                  api.setValue([...api.value, firstValue]);
+                }
+              }}
+              className="w-auto"
+            >
+              <C.Control>
+                <TagsInput.Input placeholder="Add Framework" asChild>
+                  <C.Input placeholder="select a framework" unstyled />
+                </TagsInput.Input>
+                <C.Trigger asChild>
+                  <ChevronsUpDownIcon />
+                </C.Trigger>
+              </C.Control>
+              <Portal>
+                <C.Positioner>
+                  <C.Content>
+                    <C.ItemGroup id="framework">
+                      <C.ItemGroupLabel htmlFor="framework">
+                        Frameworks
+                      </C.ItemGroupLabel>
+                      {items.map((item) => (
+                        <C.Item key={item.value} item={item}>
+                          <C.ItemText>{item.label}</C.ItemText>
+                          <C.ItemIndicator>✓</C.ItemIndicator>
+                        </C.Item>
+                      ))}
+                    </C.ItemGroup>
+                  </C.Content>
+                </C.Positioner>
+              </Portal>
+            </C>
+          </TagsInput.Control>
+        </>
+      )}
+    </TagsInput>
+  );
+};
+
 const Disabled = () => {
   return (
     <TagsInput disabled defaultValue={["test"]}>
@@ -98,6 +173,7 @@ export const TagsInputs = () => {
       <Normal />
       <WithInputGroup />
       <Disabled />
+      <WithCombobox />
     </Story>
   );
 };
