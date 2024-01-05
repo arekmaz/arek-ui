6387174bc1709140f85fcff1ce7ca4a9365b83e3
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-01-05 12:27:16 +0100

    --wip-- [skip ci]

--- app/demo/button.stories.tsx
+++ app/demo/button.stories.tsx
@@ -7,16 +7,31 @@ import {
   ButtonSecondary,
 } from "~/components/ui/button";
 import { Story } from "./storyHelpers";
+import { Loader2, Mail } from "lucide-react";
 
 export const Buttons = () => {
   return (
     <Story title="buttons">
       <Button>Primary</Button>
+
       <ButtonSecondary>Secondary</ButtonSecondary>
+
       <ButtonGhost>Ghost</ButtonGhost>
+
       <ButtonDestructive>Destructive</ButtonDestructive>
+
       <ButtonLink>Link</ButtonLink>
+
       <ButtonOutline>Outline</ButtonOutline>
+
+      <Button>
+        <Mail className="mr-2 h-4 w-4" /> Login with Email
+      </Button>
+
+      <Button disabled>
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        Please wait
+      </Button>
     </Story>
   );
 };
