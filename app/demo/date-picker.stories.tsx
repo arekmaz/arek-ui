659efb77ca74c28d04715de9b18e6ac9a41e9f3d
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-02-08 13:55:13 +0100

    --wip-- [skip ci]

--- app/demo/date-picker.stories.tsx
+++ app/demo/date-picker.stories.tsx
@@ -582,6 +582,160 @@ export const WithInputGroup = (props: {}) => {
   );
 };
 
+export const ReadonlyInput = (props: {}) => {
+  return (
+    <D
+      positioning={{ sameWidth: true }}
+      startOfWeek={1}
+      selectionMode="range"
+      {...props}
+    >
+      <D.Label>With Input Group - readonly input</D.Label>
+      <D.Control>
+        <D.Trigger asChild>
+          <InputGroup>
+            <D.Input asChild readOnly>
+              <InputGroup.Input />
+            </D.Input>
+            <InputGroup.RightAddon>
+              <IconButton unstyled aria-label="Open date picker">
+                <CalendarIcon />
+              </IconButton>
+            </InputGroup.RightAddon>
+          </InputGroup>
+        </D.Trigger>
+      </D.Control>
+      <D.Positioner>
+        <D.Content>
+          <D.View view="day">
+            {(api) => (
+              <>
+                <D.ViewControl>
+                  <D.PrevTrigger asChild>
+                    <IconButton variant="ghost" size="sm">
+                      <ChevronLeftIcon />
+                    </IconButton>
+                  </D.PrevTrigger>
+                  <D.ViewTrigger asChild>
+                    <Button variant="ghost" size="sm">
+                      <D.RangeText />
+                    </Button>
+                  </D.ViewTrigger>
+                  <D.NextTrigger asChild>
+                    <IconButton variant="ghost" size="sm">
+                      <ChevronRightIcon />
+                    </IconButton>
+                  </D.NextTrigger>
+                </D.ViewControl>
+                <D.Table>
+                  <D.TableHead>
+                    <D.TableRow>
+                      {api.weekDays.map((weekDay, id) => (
+                        <D.TableHeader key={id}>{weekDay.narrow}</D.TableHeader>
+                      ))}
+                    </D.TableRow>
+                  </D.TableHead>
+                  <D.TableBody>
+                    {api.weeks.map((week, id) => (
+                      <D.TableRow key={id}>
+                        {week.map((day, id) => (
+                          <D.TableCell key={id} value={day}>
+                            <D.TableCellTrigger asChild>
+                              <IconButton variant="ghost">{day.day}</IconButton>
+                            </D.TableCellTrigger>
+                          </D.TableCell>
+                        ))}
+                      </D.TableRow>
+                    ))}
+                  </D.TableBody>
+                </D.Table>
+              </>
+            )}
+          </D.View>
+          <D.View view="month">
+            {(api) => (
+              <>
+                <D.ViewControl>
+                  <D.PrevTrigger asChild>
+                    <IconButton variant="ghost" size="sm">
+                      <ChevronLeftIcon />
+                    </IconButton>
+                  </D.PrevTrigger>
+                  <D.ViewTrigger asChild>
+                    <Button variant="ghost" size="sm">
+                      <D.RangeText />
+                    </Button>
+                  </D.ViewTrigger>
+                  <D.NextTrigger asChild>
+                    <IconButton variant="ghost" size="sm">
+                      <ChevronRightIcon />
+                    </IconButton>
+                  </D.NextTrigger>
+                </D.ViewControl>
+                <D.Table>
+                  <D.TableBody>
+                    {api
+                      .getMonthsGrid({ columns: 4, format: "short" })
+                      .map((months, id) => (
+                        <D.TableRow key={id}>
+                          {months.map((month, id) => (
+                            <D.TableCell key={id} value={month.value}>
+                              <D.TableCellTrigger asChild>
+                                <Button variant="ghost">{month.label}</Button>
+                              </D.TableCellTrigger>
+                            </D.TableCell>
+                          ))}
+                        </D.TableRow>
+                      ))}
+                  </D.TableBody>
+                </D.Table>
+              </>
+            )}
+          </D.View>
+          <D.View view="year">
+            {(api) => (
+              <>
+                <D.ViewControl>
+                  <D.PrevTrigger asChild>
+                    <IconButton variant="ghost" size="sm">
+                      <ChevronLeftIcon />
+                    </IconButton>
+                  </D.PrevTrigger>
+                  <D.ViewTrigger asChild>
+                    <Button variant="ghost" size="sm">
+                      <D.RangeText />
+                    </Button>
+                  </D.ViewTrigger>
+                  <D.NextTrigger asChild>
+                    <IconButton variant="ghost" size="sm">
+                      <ChevronRightIcon />
+                    </IconButton>
+                  </D.NextTrigger>
+                </D.ViewControl>
+                <D.Table>
+                  <D.TableBody>
+                    {api.getYearsGrid({ columns: 4 }).map((years, id) => (
+                      <D.TableRow key={id}>
+                        {years.map((year, id) => (
+                          <D.TableCell key={id} value={year.value}>
+                            <D.TableCellTrigger asChild>
+                              <Button variant="ghost">{year.label}</Button>
+                            </D.TableCellTrigger>
+                          </D.TableCell>
+                        ))}
+                      </D.TableRow>
+                    ))}
+                  </D.TableBody>
+                </D.Table>
+              </>
+            )}
+          </D.View>
+        </D.Content>
+      </D.Positioner>
+    </D>
+  );
+};
+
 export const DatePickers = () => {
   return (
     <Story title="date pickers" componentFilename="date-picker">
@@ -589,6 +743,7 @@ export const DatePickers = () => {
       <BasicDatePicker />
       <ParkDatePicker />
       <WithInputGroup />
+      <ReadonlyInput />
     </Story>
   );
 };
--- app/routes/wip.tsx
+++ app/routes/wip.tsx
@@ -1,11 +1,12 @@
 import { Center } from "~/components/ui/stack";
+import { DatePickers } from "~/demo/date-picker.stories";
 import { Progresses } from "~/demo/progress.stories";
 import { TreeViews } from "~/demo/tree-view.stories";
 
 export default function Wip() {
   return (
     <Center className="flex-1">
-      <Progresses />
+      <DatePickers />
     </Center>
   );
 }
