1ba51c78758162369dd91ffd19fe7030c62138d9
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-02-11 14:25:45 +0100

    --wip-- [skip ci]

--- app/stories/scroll-area.stories.tsx
+++ app/stories/scroll-area.stories.tsx
@@ -1,32 +1,45 @@
 import { Separator } from "~/components/ui/separator";
 import { ScrollArea } from "~/components/ui/scroll-area";
-import { Story } from "./storyHelpers";
 import { Fragment } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "ScrollArea",
+  component: ScrollArea,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    // backgroundColor: { control: "color" },
+  },
+} satisfies Meta<typeof ScrollArea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
 
 const tags = Array.from({ length: 50 }).map(
   (_, i, a) => `v1.2.0-beta.${a.length - i}`
 );
 
-export function ScrollAreaDemo() {
-  return (
-    <ScrollArea className="h-72 w-48 rounded-md border">
-      <div className="p-4">
-        <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
-        {tags.map((tag) => (
-          <Fragment key={tag}>
-            <div className="text-sm">{tag}</div>
-            <Separator className="my-2" />
-          </Fragment>
-        ))}
-      </div>
-    </ScrollArea>
-  );
-}
-
-export const ScrollAreas = () => {
-  return (
-    <Story title="scroll areas" componentFilename="scroll-area">
-      <ScrollAreaDemo />
-    </Story>
-  );
+export const Demo: Story = {
+  render: () => {
+    return (
+      <ScrollArea className="h-72 w-48 rounded-md border">
+        <div className="p-4">
+          <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
+          {tags.map((tag) => (
+            <Fragment key={tag}>
+              <div className="text-sm">{tag}</div>
+              <Separator className="my-2" />
+            </Fragment>
+          ))}
+        </div>
+      </ScrollArea>
+    );
+  },
 };
--- app/stories/select.stories.tsx
+++ app/stories/select.stories.tsx
@@ -3,254 +3,271 @@ import { ChevronsUpDownIcon, SearchIcon } from "lucide-react";
 import { matchSorter } from "match-sorter";
 import { useState } from "react";
 import { Select as S, data } from "~/components/ui/select";
-import { Story } from "./storyHelpers";
 import { InputGroup } from "~/components/ui/input-group";
 import { HStack } from "~/components/ui/stack";
+import type { Meta, StoryObj } from "@storybook/react";
 
-export const MultipleSelect = () => {
-  const [items, setItems] = useState(data);
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Select",
+  component: Select,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    // backgroundColor: { control: "color" },
+  },
+} satisfies Meta<typeof Select>;
 
-  const handleChange = (e: any) => {
-    const filtered = matchSorter(data, e.value, { keys: ["label"] });
-    setItems(filtered.length > 0 ? filtered : data);
-  };
+export default meta;
+type Story = StoryObj<typeof meta>;
 
-  return (
-    <S items={items} multiple>
-      <S.Label>Multiple select</S.Label>
-      <S.Control>
-        <S.Trigger>
-          <S.ValueText placeholder="select a framework" />
-          <S.Indicator asChild>
-            <ChevronsUpDownIcon />
-          </S.Indicator>
-        </S.Trigger>
-      </S.Control>
-      <Portal>
-        <S.Positioner>
-          <S.Content>
-            <S.AutoFocusingFilterInput
-              placeholder="Filter frameworks"
-              onChange={(e) => handleChange(e.target)}
-              className="pointer-events-auto"
-            />
-            <S.ItemGroup id="framework">
-              <S.ItemGroupLabel htmlFor="framework">
-                Frameworks
-              </S.ItemGroupLabel>
-              {items.map((item) => (
-                <S.Item key={item.value} item={item}>
-                  <S.ItemText>{item.label}</S.ItemText>
-                  <S.ItemIndicator>✓</S.ItemIndicator>
-                </S.Item>
-              ))}
-            </S.ItemGroup>
-          </S.Content>
-        </S.Positioner>
-      </Portal>
-    </S>
-  );
+export const MultipleSelect: Story = {
+  render: () => {
+    const [items, setItems] = useState(data);
+
+    const handleChange = (e: any) => {
+      const filtered = matchSorter(data, e.value, { keys: ["label"] });
+      setItems(filtered.length > 0 ? filtered : data);
+    };
+
+    return (
+      <S items={items} multiple>
+        <S.Label>Multiple select</S.Label>
+        <S.Control>
+          <S.Trigger>
+            <S.ValueText placeholder="select a framework" />
+            <S.Indicator asChild>
+              <ChevronsUpDownIcon />
+            </S.Indicator>
+          </S.Trigger>
+        </S.Control>
+        <Portal>
+          <S.Positioner>
+            <S.Content>
+              <S.AutoFocusingFilterInput
+                placeholder="Filter frameworks"
+                onChange={(e) => handleChange(e.target)}
+                className="pointer-events-auto"
+              />
+              <S.ItemGroup id="framework">
+                <S.ItemGroupLabel htmlFor="framework">
+                  Frameworks
+                </S.ItemGroupLabel>
+                {items.map((item) => (
+                  <S.Item key={item.value} item={item}>
+                    <S.ItemText>{item.label}</S.ItemText>
+                    <S.ItemIndicator>✓</S.ItemIndicator>
+                  </S.Item>
+                ))}
+              </S.ItemGroup>
+            </S.Content>
+          </S.Positioner>
+        </Portal>
+      </S>
+    );
+  },
 };
 
-export const SingleSelect = () => {
-  const [items, setItems] = useState(data);
+export const SingleSelect: Story = {
+  render: () => {
+    const [items, setItems] = useState(data);
 
-  const handleChange = (e: any) => {
-    const filtered = matchSorter(data, e.value, { keys: ["label"] });
-    setItems(filtered.length > 0 ? filtered : data);
-  };
+    const handleChange = (e: any) => {
+      const filtered = matchSorter(data, e.value, { keys: ["label"] });
+      setItems(filtered.length > 0 ? filtered : data);
+    };
 
-  return (
-    <S items={items}>
-      <S.Label>Single select</S.Label>
-      <S.Control>
-        <S.Trigger>
-          <S.ValueText placeholder="select a framework" />
-          <S.Indicator asChild>
-            <ChevronsUpDownIcon />
-          </S.Indicator>
-        </S.Trigger>
-      </S.Control>
-      <Portal>
-        <S.Positioner>
-          <S.Content>
-            <S.AutoFocusingFilterInput
-              placeholder="Filter frameworks"
-              onChange={(e) => handleChange(e.target)}
-              className="pointer-events-auto"
-            />
-            <S.ItemGroup id="framework">
-              <S.ItemGroupLabel htmlFor="framework">
-                Frameworks
-              </S.ItemGroupLabel>
-              {items.map((item) => (
-                <S.Item key={item.value} item={item}>
-                  <S.ItemText>{item.label}</S.ItemText>
-                  <S.ItemIndicator>✓</S.ItemIndicator>
-                </S.Item>
-              ))}
-            </S.ItemGroup>
-          </S.Content>
-        </S.Positioner>
-      </Portal>
-    </S>
-  );
+    return (
+      <S items={items}>
+        <S.Label>Single select</S.Label>
+        <S.Control>
+          <S.Trigger>
+            <S.ValueText placeholder="select a framework" />
+            <S.Indicator asChild>
+              <ChevronsUpDownIcon />
+            </S.Indicator>
+          </S.Trigger>
+        </S.Control>
+        <Portal>
+          <S.Positioner>
+            <S.Content>
+              <S.AutoFocusingFilterInput
+                placeholder="Filter frameworks"
+                onChange={(e) => handleChange(e.target)}
+                className="pointer-events-auto"
+              />
+              <S.ItemGroup id="framework">
+                <S.ItemGroupLabel htmlFor="framework">
+                  Frameworks
+                </S.ItemGroupLabel>
+                {items.map((item) => (
+                  <S.Item key={item.value} item={item}>
+                    <S.ItemText>{item.label}</S.ItemText>
+                    <S.ItemIndicator>✓</S.ItemIndicator>
+                  </S.Item>
+                ))}
+              </S.ItemGroup>
+            </S.Content>
+          </S.Positioner>
+        </Portal>
+      </S>
+    );
+  },
 };
 
-export const WithError = () => {
-  const [items, setItems] = useState(data);
+export const WithError: Story = {
+  render: () => {
+    const [items, setItems] = useState(data);
 
-  const handleChange = (e: any) => {
-    const filtered = matchSorter(data, e.value, { keys: ["label"] });
-    setItems(filtered.length > 0 ? filtered : data);
-  };
+    const handleChange = (e: any) => {
+      const filtered = matchSorter(data, e.value, { keys: ["label"] });
+      setItems(filtered.length > 0 ? filtered : data);
+    };
 
-  return (
-    <S items={items} variant="error">
-      <S.Label>With error</S.Label>
-      <S.Control>
-        <S.Trigger>
-          <S.ValueText placeholder="select a framework" />
-          <S.Indicator asChild>
-            <ChevronsUpDownIcon />
-          </S.Indicator>
-        </S.Trigger>
-      </S.Control>
-      <Portal>
-        <S.Positioner>
-          <S.Content>
-            <S.AutoFocusingFilterInput
-              placeholder="Filter frameworks"
-              onChange={(e) => handleChange(e.target)}
-              className="pointer-events-auto"
-            />
-            <S.ItemGroup id="framework">
-              <S.ItemGroupLabel htmlFor="framework">
-                Frameworks
-              </S.ItemGroupLabel>
-              {items.map((item) => (
-                <S.Item key={item.value} item={item}>
-                  <S.ItemText>{item.label}</S.ItemText>
-                  <S.ItemIndicator>✓</S.ItemIndicator>
-                </S.Item>
-              ))}
-            </S.ItemGroup>
-          </S.Content>
-        </S.Positioner>
-      </Portal>
-    </S>
-  );
+    return (
+      <S items={items} variant="error">
+        <S.Label>With error</S.Label>
+        <S.Control>
+          <S.Trigger>
+            <S.ValueText placeholder="select a framework" />
+            <S.Indicator asChild>
+              <ChevronsUpDownIcon />
+            </S.Indicator>
+          </S.Trigger>
+        </S.Control>
+        <Portal>
+          <S.Positioner>
+            <S.Content>
+              <S.AutoFocusingFilterInput
+                placeholder="Filter frameworks"
+                onChange={(e) => handleChange(e.target)}
+                className="pointer-events-auto"
+              />
+              <S.ItemGroup id="framework">
+                <S.ItemGroupLabel htmlFor="framework">
+                  Frameworks
+                </S.ItemGroupLabel>
+                {items.map((item) => (
+                  <S.Item key={item.value} item={item}>
+                    <S.ItemText>{item.label}</S.ItemText>
+                    <S.ItemIndicator>✓</S.ItemIndicator>
+                  </S.Item>
+                ))}
+              </S.ItemGroup>
+            </S.Content>
+          </S.Positioner>
+        </Portal>
+      </S>
+    );
+  },
 };
 
-export const DisabledSelect = () => {
-  const [items, setItems] = useState(data);
+export const DisabledSelect: Story = {
+  render: () => {
+    const [items, setItems] = useState(data);
 
-  const handleChange = (e: any) => {
-    const filtered = matchSorter(data, e.value, { keys: ["label"] });
-    setItems(filtered.length > 0 ? filtered : data);
-  };
+    const handleChange = (e: any) => {
+      const filtered = matchSorter(data, e.value, { keys: ["label"] });
+      setItems(filtered.length > 0 ? filtered : data);
+    };
 
-  return (
-    <S items={items} disabled>
-      <S.Label>Disabled select</S.Label>
-      <S.Control>
-        <S.Trigger>
-          <S.ValueText placeholder="select a framework" />
-          <S.Indicator asChild>
-            <ChevronsUpDownIcon />
-          </S.Indicator>
-        </S.Trigger>
-      </S.Control>
-      <Portal>
-        <S.Positioner>
-          <S.Content>
-            <S.AutoFocusingFilterInput
-              placeholder="Filter frameworks"
-              onChange={(e) => handleChange(e.target)}
-              className="pointer-events-auto"
-            />
-            <S.ItemGroup id="framework">
-              <S.ItemGroupLabel htmlFor="framework">
-                Frameworks
-              </S.ItemGroupLabel>
-              {items.map((item) => (
-                <S.Item key={item.value} item={item}>
-                  <S.ItemText>{item.label}</S.ItemText>
-                  <S.ItemIndicator>✓</S.ItemIndicator>
-                </S.Item>
-              ))}
-            </S.ItemGroup>
-          </S.Content>
-        </S.Positioner>
-      </Portal>
-    </S>
-  );
+    return (
+      <S items={items} disabled>
+        <S.Label>Disabled select</S.Label>
+        <S.Control>
+          <S.Trigger>
+            <S.ValueText placeholder="select a framework" />
+            <S.Indicator asChild>
+              <ChevronsUpDownIcon />
+            </S.Indicator>
+          </S.Trigger>
+        </S.Control>
+        <Portal>
+          <S.Positioner>
+            <S.Content>
+              <S.AutoFocusingFilterInput
+                placeholder="Filter frameworks"
+                onChange={(e) => handleChange(e.target)}
+                className="pointer-events-auto"
+              />
+              <S.ItemGroup id="framework">
+                <S.ItemGroupLabel htmlFor="framework">
+                  Frameworks
+                </S.ItemGroupLabel>
+                {items.map((item) => (
+                  <S.Item key={item.value} item={item}>
+                    <S.ItemText>{item.label}</S.ItemText>
+                    <S.ItemIndicator>✓</S.ItemIndicator>
+                  </S.Item>
+                ))}
+              </S.ItemGroup>
+            </S.Content>
+          </S.Positioner>
+        </Portal>
+      </S>
+    );
+  },
 };
 
-export const WithGroupInput = () => {
-  const [search, setSearch] = useState("");
+export const WithGroupInput: Story = {
+  render: () => {
+    const [search, setSearch] = useState("");
 
-  const items = matchSorter(data, search, { keys: ["label"] });
+    const items = matchSorter(data, search, { keys: ["label"] });
 
-  return (
-    <S items={items} multiple onValueChange={() => setSearch("")}>
-      <S.Label>With input group</S.Label>
-      <S.Control>
-        <S.Trigger>
-          <S.ValueText placeholder="select a framework" />
-          <S.Indicator asChild>
-            <ChevronsUpDownIcon />
-          </S.Indicator>
-        </S.Trigger>
-      </S.Control>
-      <Portal>
-        <S.Positioner>
-          <S.Content>
-            <InputGroup
-              asChild
-              variant="ghost"
-              className="px-2 py-1 border-b h-10"
-            >
-              <HStack>
-                <InputGroup.LeftAddon>
-                  <SearchIcon />
-                </InputGroup.LeftAddon>
-                <InputGroup.Input asChild>
-                  <S.AutoFocusingFilterInput
-                    unstyled
-                    placeholder="Filter frameworks"
-                    onChange={(e) => setSearch(e.target.value)}
-                    value={search}
-                  />
-                </InputGroup.Input>
-              </HStack>
-            </InputGroup>
-            <S.ItemGroup id="framework">
-              <S.ItemGroupLabel htmlFor="framework">
-                Frameworks
-              </S.ItemGroupLabel>
-              {items.map((item) => (
-                <S.Item key={item.value} item={item}>
-                  <S.ItemText>{item.label}</S.ItemText>
-                  <S.ItemIndicator>✓</S.ItemIndicator>
-                </S.Item>
-              ))}
-            </S.ItemGroup>
-          </S.Content>
-        </S.Positioner>
-      </Portal>
-    </S>
-  );
-};
-
-export const Selects = () => {
-  return (
-    <Story title="selects" componentFilename="select">
-      <SingleSelect />
-      <MultipleSelect />
-      <DisabledSelect />
-      <WithGroupInput />
-      <WithError />
-    </Story>
-  );
+    return (
+      <S items={items} multiple onValueChange={() => setSearch("")}>
+        <S.Label>With input group</S.Label>
+        <S.Control>
+          <S.Trigger>
+            <S.ValueText placeholder="select a framework" />
+            <S.Indicator asChild>
+              <ChevronsUpDownIcon />
+            </S.Indicator>
+          </S.Trigger>
+        </S.Control>
+        <Portal>
+          <S.Positioner>
+            <S.Content>
+              <InputGroup
+                asChild
+                variant="ghost"
+                className="px-2 py-1 border-b h-10"
+              >
+                <HStack>
+                  <InputGroup.LeftAddon>
+                    <SearchIcon />
+                  </InputGroup.LeftAddon>
+                  <InputGroup.Input asChild>
+                    <S.AutoFocusingFilterInput
+                      unstyled
+                      placeholder="Filter frameworks"
+                      onChange={(e) => setSearch(e.target.value)}
+                      value={search}
+                    />
+                  </InputGroup.Input>
+                </HStack>
+              </InputGroup>
+              <S.ItemGroup id="framework">
+                <S.ItemGroupLabel htmlFor="framework">
+                  Frameworks
+                </S.ItemGroupLabel>
+                {items.map((item) => (
+                  <S.Item key={item.value} item={item}>
+                    <S.ItemText>{item.label}</S.ItemText>
+                    <S.ItemIndicator>✓</S.ItemIndicator>
+                  </S.Item>
+                ))}
+              </S.ItemGroup>
+            </S.Content>
+          </S.Positioner>
+        </Portal>
+      </S>
+    );
+  },
 };
