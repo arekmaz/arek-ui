d1ea6d7b2f0ebb00e48125058c58f68d9c984c8c
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-02-11 14:31:00 +0100

    --wip-- [skip ci]

--- app/stories/skeleton.stories.tsx
+++ app/stories/skeleton.stories.tsx
@@ -1,62 +1,77 @@
 import { Skeleton } from "~/components/ui/skeleton";
-import { Story } from "./storyHelpers";
 import { HStack, Stack } from "~/components/ui/stack";
+import type { Meta, StoryObj } from "@storybook/react";
 
-export function SkeletonDemo() {
-  return (
-    <div className="flex items-center space-x-4">
-      <Skeleton className="size-12" variant="circle" />
-      <Stack spacing={2}>
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
-      </Stack>
-    </div>
-  );
-}
-const List = () => {
-  return (
-    <Stack spacing={2} align="stretch" className="w-full">
-      <HStack>
-        <Skeleton className="h-2 flex-1" />
-        <Skeleton className="h-2 flex-[5]" />
-      </HStack>
-      <HStack>
-        <Skeleton className="h-2 flex-[3]" />
-        <Skeleton className="h-2 flex-[2]" />
-      </HStack>
-      <HStack>
-        <Skeleton className="h-2 flex-[4]" />
-        <Skeleton className="h-2 flex-1" />
-      </HStack>
-      <HStack>
-        <Skeleton className="h-2 flex-[3]" />
-        <Skeleton className="h-2 flex-[8]" />
-      </HStack>
-      <HStack>
-        <Skeleton className="h-2 flex-1" />
-        <Skeleton className="h-2 flex-[5]" />
-      </HStack>
-      <HStack>
-        <Skeleton className="h-2 flex-[3]" />
-        <Skeleton className="h-2 flex-[2]" />
-      </HStack>
-      <HStack>
-        <Skeleton className="h-2 flex-[4]" />
-        <Skeleton className="h-2 flex-1" />
-      </HStack>
-      <HStack>
-        <Skeleton className="h-2 flex-[3]" />
-        <Skeleton className="h-2 flex-[8]" />
-      </HStack>
-    </Stack>
-  );
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Skeleton",
+  component: Skeleton,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    // backgroundColor: { control: "color" },
+  },
+} satisfies Meta<typeof Skeleton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const SkeletonDemo: Story = {
+  render: () => {
+    return (
+      <div className="flex items-center space-x-4">
+        <Skeleton className="size-12" variant="circle" />
+        <Stack spacing={2}>
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </Stack>
+      </div>
+    );
+  },
 };
 
-export const Skeletons = () => {
-  return (
-    <Story title="skeletons" componentFilename="skeleton">
-      <SkeletonDemo />
-      <List />
-    </Story>
-  );
+export const List: Story = {
+  render: () => {
+    return (
+      <Stack spacing={2} align="stretch" className="w-full">
+        <HStack>
+          <Skeleton className="h-2 flex-1" />
+          <Skeleton className="h-2 flex-[5]" />
+        </HStack>
+        <HStack>
+          <Skeleton className="h-2 flex-[3]" />
+          <Skeleton className="h-2 flex-[2]" />
+        </HStack>
+        <HStack>
+          <Skeleton className="h-2 flex-[4]" />
+          <Skeleton className="h-2 flex-1" />
+        </HStack>
+        <HStack>
+          <Skeleton className="h-2 flex-[3]" />
+          <Skeleton className="h-2 flex-[8]" />
+        </HStack>
+        <HStack>
+          <Skeleton className="h-2 flex-1" />
+          <Skeleton className="h-2 flex-[5]" />
+        </HStack>
+        <HStack>
+          <Skeleton className="h-2 flex-[3]" />
+          <Skeleton className="h-2 flex-[2]" />
+        </HStack>
+        <HStack>
+          <Skeleton className="h-2 flex-[4]" />
+          <Skeleton className="h-2 flex-1" />
+        </HStack>
+        <HStack>
+          <Skeleton className="h-2 flex-[3]" />
+          <Skeleton className="h-2 flex-[8]" />
+        </HStack>
+      </Stack>
+    );
+  },
 };
--- app/stories/slider.stories.tsx
+++ app/stories/slider.stories.tsx
@@ -1,8 +1,27 @@
 import { Slider, SliderProps } from "~/components/ui/slider";
-import { Story } from "./storyHelpers";
 import { useState } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 
-export const Single = (props: SliderProps) => {
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Slider",
+  component: Slider,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    // backgroundColor: { control: "color" },
+  },
+} satisfies Meta<typeof Slider>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const Single = (props: SliderProps) => {
   const [value, setValue] = useState([33]);
 
   return (
@@ -31,81 +50,85 @@ export const Single = (props: SliderProps) => {
   );
 };
 
-export const Range = (props: SliderProps) => {
-  const [value, setValue] = useState([33, 55]);
-
-  return (
-    <Slider
-      min={0}
-      max={100}
-      value={value}
-      onValueChange={(value) => setValue(value.value)}
-      {...props}
-    >
-      <Slider.Label>
-        Range {props.size ?? "md"}: {value.join(" - ")}
-      </Slider.Label>
-      <Slider.Control>
-        <Slider.Track>
-          <Slider.Range />
-        </Slider.Track>
-        <Slider.Thumb index={0} />
-        <Slider.Thumb index={1} />
-      </Slider.Control>
-      <Slider.MarkerGroup>
-        <Slider.Marker value={25}>25</Slider.Marker>
-        <Slider.Marker value={50}>50</Slider.Marker>
-        <Slider.Marker value={75}>75</Slider.Marker>
-      </Slider.MarkerGroup>
-    </Slider>
-  );
+export const SingleSlider: Story = {
+  render: Single,
 };
 
-export const Range3 = (props: SliderProps) => {
-  const [value, setValue] = useState([33, 55, 66]);
-
-  return (
-    <Slider
-      min={0}
-      max={100}
-      value={value}
-      onValueChange={(value) => setValue(value.value)}
-      {...props}
-    >
-      <Slider.Label>
-        Range 3 {props.size ?? "md"}: {value.join(" - ")}
-      </Slider.Label>
-      <Slider.Control>
-        <Slider.Track>
-          <Slider.Range />
-        </Slider.Track>
-        <Slider.Thumb index={0} />
-        <Slider.Thumb index={1} />
-        <Slider.Thumb index={2} />
-      </Slider.Control>
-      <Slider.MarkerGroup>
-        <Slider.Marker value={25}>25</Slider.Marker>
-        <Slider.Marker value={50}>50</Slider.Marker>
-        <Slider.Marker value={75}>75</Slider.Marker>
-      </Slider.MarkerGroup>
-    </Slider>
-  );
+export const SingleSliderSM: Story = {
+  args: {
+    size: "sm",
+  },
+  render: Single,
 };
 
-export const Sliders = () => {
-  return (
-    <Story
-      title="sliders"
-      classes={{ content: "min-w-80 gap-10 pb-10" }}
-      componentFilename="slider"
-    >
-      <Single size="sm" />
-      <Single />
-      <Single size="lg" />
+export const SingleSliderLG: Story = {
+  args: {
+    size: "lg",
+  },
+  render: Single,
+};
 
-      <Range />
+export const Range: Story = {
+  render: (props) => {
+    const [value, setValue] = useState([33, 55]);
 
-      <Range3 />
-    </Story>
-  );
+    return (
+      <Slider
+        min={0}
+        max={100}
+        value={value}
+        onValueChange={(value) => setValue(value.value)}
+        {...props}
+      >
+        <Slider.Label>
+          Range {props.size ?? "md"}: {value.join(" - ")}
+        </Slider.Label>
+        <Slider.Control>
+          <Slider.Track>
+            <Slider.Range />
+          </Slider.Track>
+          <Slider.Thumb index={0} />
+          <Slider.Thumb index={1} />
+        </Slider.Control>
+        <Slider.MarkerGroup>
+          <Slider.Marker value={25}>25</Slider.Marker>
+          <Slider.Marker value={50}>50</Slider.Marker>
+          <Slider.Marker value={75}>75</Slider.Marker>
+        </Slider.MarkerGroup>
+      </Slider>
+    );
+  },
+};
+
+export const Range3: Story = {
+  render: (props) => {
+    const [value, setValue] = useState([33, 55, 66]);
+
+    return (
+      <Slider
+        min={0}
+        max={100}
+        value={value}
+        onValueChange={(value) => setValue(value.value)}
+        {...props}
+      >
+        <Slider.Label>
+          Range 3 {props.size ?? "md"}: {value.join(" - ")}
+        </Slider.Label>
+        <Slider.Control>
+          <Slider.Track>
+            <Slider.Range />
+          </Slider.Track>
+          <Slider.Thumb index={0} />
+          <Slider.Thumb index={1} />
+          <Slider.Thumb index={2} />
+        </Slider.Control>
+        <Slider.MarkerGroup>
+          <Slider.Marker value={25}>25</Slider.Marker>
+          <Slider.Marker value={50}>50</Slider.Marker>
+          <Slider.Marker value={75}>75</Slider.Marker>
+        </Slider.MarkerGroup>
+      </Slider>
+    );
+  },
 };
