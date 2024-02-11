63d5a91698b46839f5c8778519238a15c12006ef
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-02-11 14:16:22 +0100

    --wip-- [skip ci]

--- app/stories/menu.stories.tsx
+++ app/stories/menu.stories.tsx
@@ -9,98 +9,107 @@ import {
   UserIcon,
   UserPlusIcon,
 } from "lucide-react";
-import { ComponentProps } from "react";
 import { Button } from "~/components/ui/button";
 import { Menu } from "~/components/ui/menu";
 import { HStack } from "~/components/ui/stack";
-import { Story } from "./storyHelpers";
+import type { Meta, StoryObj } from "@storybook/react";
 
-export const Demo = (props: ComponentProps<typeof Menu>) => {
-  return (
-    <Menu {...props}>
-      <Menu.Trigger asChild>
-        <Button variant="outline">Open Menu</Button>
-      </Menu.Trigger>
-      <Menu.Positioner>
-        <Menu.Content>
-          <Menu.ItemGroup id="group-1">
-            <Menu.ItemGroupLabel htmlFor="group-1">
-              My Account
-            </Menu.ItemGroupLabel>
-            <Menu.Separator />
-            <Menu.Item id="profile">
-              <HStack spacing={6} justify="between" className="flex-1">
-                <HStack spacing={2}>
-                  <UserIcon />
-                  Profile
-                </HStack>
-                <span className="text-muted-foreground text-sm">⇧⌘P</span>
-              </HStack>
-            </Menu.Item>
-            <Menu.Item id="billing">
-              <HStack spacing={2}>
-                <CreditCardIcon /> Billing
-              </HStack>
-            </Menu.Item>
-            <Menu.Item id="settings">
-              <HStack spacing={6} justify="between" className="flex-1">
-                <HStack spacing={2}>
-                  <SettingsIcon /> Settings
-                </HStack>
-                <span className="text-muted-foreground text-sm">⌘,</span>
-              </HStack>
-            </Menu.Item>
-            <Menu.Root
-              positioning={{ placement: "right-start", gutter: -2 }}
-              {...props}
-            >
-              <Menu.TriggerItem className="justify-between">
-                <HStack spacing={2}>
-                  <UserPlusIcon />
-                  Invite member
-                </HStack>
-                <ChevronRightIcon />
-              </Menu.TriggerItem>
-              <Menu.Positioner>
-                <Menu.Content>
-                  <Menu.Item id="email">
-                    <HStack spacing={2}>
-                      <MailIcon /> Email
-                    </HStack>
-                  </Menu.Item>
-                  <Menu.Item id="message">
-                    <HStack spacing={2}>
-                      <MessageSquareIcon /> Message
-                    </HStack>
-                  </Menu.Item>
-                  <Menu.Separator />
-                  <Menu.Item id="other">
-                    <HStack spacing={2}>
-                      <PlusCircleIcon />
-                      More Options...
-                    </HStack>
-                  </Menu.Item>
-                </Menu.Content>
-              </Menu.Positioner>
-            </Menu.Root>
-            <Menu.Separator />
-            <Menu.Item id="logout">
-              <HStack spacing={2}>
-                <LogOutIcon />
-                Logout
-              </HStack>
-            </Menu.Item>
-          </Menu.ItemGroup>
-        </Menu.Content>
-      </Menu.Positioner>
-    </Menu>
-  );
-};
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Menu",
+  component: Menu,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    // backgroundColor: { control: "color" },
+  },
+} satisfies Meta<typeof Menu>;
 
-export const Menus = () => {
-  return (
-    <Story title="menus" componentFilename="menu">
-      <Demo />
-    </Story>
-  );
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Demo: Story = {
+  render: () => {
+    return (
+      <Menu>
+        <Menu.Trigger asChild>
+          <Button variant="outline">Open Menu</Button>
+        </Menu.Trigger>
+        <Menu.Positioner>
+          <Menu.Content>
+            <Menu.ItemGroup id="group-1">
+              <Menu.ItemGroupLabel htmlFor="group-1">
+                My Account
+              </Menu.ItemGroupLabel>
+              <Menu.Separator />
+              <Menu.Item id="profile">
+                <HStack spacing={6} justify="between" className="flex-1">
+                  <HStack spacing={2}>
+                    <UserIcon />
+                    Profile
+                  </HStack>
+                  <span className="text-muted-foreground text-sm">⇧⌘P</span>
+                </HStack>
+              </Menu.Item>
+              <Menu.Item id="billing">
+                <HStack spacing={2}>
+                  <CreditCardIcon /> Billing
+                </HStack>
+              </Menu.Item>
+              <Menu.Item id="settings">
+                <HStack spacing={6} justify="between" className="flex-1">
+                  <HStack spacing={2}>
+                    <SettingsIcon /> Settings
+                  </HStack>
+                  <span className="text-muted-foreground text-sm">⌘,</span>
+                </HStack>
+              </Menu.Item>
+              <Menu.Root positioning={{ placement: "right-start", gutter: -2 }}>
+                <Menu.TriggerItem className="justify-between">
+                  <HStack spacing={2}>
+                    <UserPlusIcon />
+                    Invite member
+                  </HStack>
+                  <ChevronRightIcon />
+                </Menu.TriggerItem>
+                <Menu.Positioner>
+                  <Menu.Content>
+                    <Menu.Item id="email">
+                      <HStack spacing={2}>
+                        <MailIcon /> Email
+                      </HStack>
+                    </Menu.Item>
+                    <Menu.Item id="message">
+                      <HStack spacing={2}>
+                        <MessageSquareIcon /> Message
+                      </HStack>
+                    </Menu.Item>
+                    <Menu.Separator />
+                    <Menu.Item id="other">
+                      <HStack spacing={2}>
+                        <PlusCircleIcon />
+                        More Options...
+                      </HStack>
+                    </Menu.Item>
+                  </Menu.Content>
+                </Menu.Positioner>
+              </Menu.Root>
+              <Menu.Separator />
+              <Menu.Item id="logout">
+                <HStack spacing={2}>
+                  <LogOutIcon />
+                  Logout
+                </HStack>
+              </Menu.Item>
+            </Menu.ItemGroup>
+          </Menu.Content>
+        </Menu.Positioner>
+      </Menu>
+    );
+  },
 };
--- app/stories/pagination.stories.tsx
+++ app/stories/pagination.stories.tsx
@@ -1,51 +1,69 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
-import { ComponentProps } from "react";
 import { Button } from "~/components/ui/button";
 import { IconButton } from "~/components/ui/icon-button";
 import { Pagination } from "~/components/ui/pagination";
-import { Story } from "./storyHelpers";
+import type { Meta, StoryObj } from "@storybook/react";
 
-export const Demo = (props: ComponentProps<typeof Pagination>) => {
-  return (
-    <Pagination pageSize={10} siblingCount={1} defaultPage={2} {...props}>
-      {({ pages, page: currentPage }) => (
-        <>
-          <Pagination.PrevTrigger asChild>
-            <IconButton variant="ghost" aria-label="Next Page">
-              <ChevronLeftIcon />
-            </IconButton>
-          </Pagination.PrevTrigger>
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Pagination",
+  component: Pagination,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    // backgroundColor: { control: "color" },
+  },
+} satisfies Meta<typeof Pagination>;
 
-          {pages.map((page, index) =>
-            page.type === "page" ? (
-              <Pagination.Item key={index} {...page} asChild>
-                <Button
-                  variant={page.value === currentPage ? "outline" : "ghost"}
-                >
-                  {page.value}
-                </Button>
-              </Pagination.Item>
-            ) : (
-              <Pagination.Ellipsis key={index} index={index}>
-                &#8230;
-              </Pagination.Ellipsis>
-            )
-          )}
-          <Pagination.NextTrigger asChild>
-            <IconButton variant="ghost" aria-label="Next Page">
-              <ChevronRightIcon />
-            </IconButton>
-          </Pagination.NextTrigger>
-        </>
-      )}
-    </Pagination>
-  );
-};
+export default meta;
+type Story = StoryObj<typeof meta>;
 
-export const Paginations = () => {
-  return (
-    <Story title="paginations" componentFilename="pagination">
-      <Demo count={90} />
-    </Story>
-  );
+export const Demo = {
+  render: (props) => {
+    return (
+      <Pagination
+        pageSize={10}
+        siblingCount={1}
+        defaultPage={2}
+        count={90}
+        {...props}
+      >
+        {({ pages, page: currentPage }) => (
+          <>
+            <Pagination.PrevTrigger asChild>
+              <IconButton variant="ghost" aria-label="Next Page">
+                <ChevronLeftIcon />
+              </IconButton>
+            </Pagination.PrevTrigger>
+
+            {pages.map((page, index) =>
+              page.type === "page" ? (
+                <Pagination.Item key={index} {...page} asChild>
+                  <Button
+                    variant={page.value === currentPage ? "outline" : "ghost"}
+                  >
+                    {page.value}
+                  </Button>
+                </Pagination.Item>
+              ) : (
+                <Pagination.Ellipsis key={index} index={index}>
+                  &#8230;
+                </Pagination.Ellipsis>
+              )
+            )}
+            <Pagination.NextTrigger asChild>
+              <IconButton variant="ghost" aria-label="Next Page">
+                <ChevronRightIcon />
+              </IconButton>
+            </Pagination.NextTrigger>
+          </>
+        )}
+      </Pagination>
+    );
+  },
 };
