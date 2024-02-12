c9722f8b6d88c2c665f228e47cd6e1fcd502f8db
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-02-12 17:42:37 +0100

    --wip-- [skip ci]

--- app/stories/popover.stories.tsx
+++ app/stories/popover.stories.tsx
@@ -12,6 +12,7 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
+    sourceLink: "popover.tsx",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
