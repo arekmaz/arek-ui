6609d291f3d3c55d1ce4c27b71fdb5eca772e0a4
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-07-05 12:41:09 +0200

    restore pnpm setup

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ WORKDIR /app
 ENV NODE_ENV="production"
 
 # Install pnpm
-ARG PNPM_VERSION=8.12.0
+ARG PNPM_VERSION=9.4.0
 RUN npm install -g pnpm@$PNPM_VERSION
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
